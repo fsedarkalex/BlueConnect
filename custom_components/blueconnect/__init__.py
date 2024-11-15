585b89ec7cd34eecf33d7f9b3af4a46db1e3e1ba
Julian Cerruti <jcerruti@gmai.com>
2024-11-15 17:37:53 -0300

    Test update

--- custom_components/blueconnect/__init__.py
+++ custom_components/blueconnect/__init__.py
@@ -3,10 +3,13 @@ from __future__ import annotations
 import asyncio
 import logging
 
+from homeassistant.const import Platform
 from homeassistant.core import Config, HomeAssistant
 from homeassistant.helpers.typing import ConfigType
+from homeassistant.config_entries import ConfigEntry
 
 DOMAIN = "blueconnect"
+PLATFORMS: list[Platform] = [Platform.SENSOR]
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -15,3 +18,14 @@ async def async_setup(hass: HomeAssistant, config: Config) -> bool:
     hass.states.async_set("blueconnect.test_state", 42)
     _LOGGER.info("blueconnect loaded")
     return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    _LOGGER.info("blueconnect entry loaded")
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
